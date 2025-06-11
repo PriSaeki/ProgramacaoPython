936776bc76aab2e06628c9884e1a9b15d9e3201f
PriSaeki <priscilasaeki@gmail.com>
2025-06-10 21:44:35 -0300

    Add files via upload

---
+++ Priscila10.06.py
@@ -0,0 +1,38 @@
+
+# x=1
+# y=5
+# for T in range (x,y+1):
+#     print(T)
+
+# x=15
+# y=0
+# t=5
+# for i in range(y,x,t):
+#     print(i) 
+
+# y=0
+# t=3
+# x=[15,1,13]
+# for i in x:
+# print(i)
+
+# salada_de_fruta=["melancia","laranja","maça","mamão","uva"]
+# for i in "salada_de_fruta":
+# print(i)
+
+# salada_de_fruta=["melancia","laranja","maça","mamão","uva"]
+# etiqueta=[1,2,3,4,5]
+# for i,j in zip(salada_de_fruta,etiqueta):
+#     print(i,j)
+
+# #1.com range
+# x=0
+# y=200
+# t=11
+# for t in range(x,y):
+#     print(t)
+
+#2.com lista
+animais=["gato","cachorro","pássaro","tigre"]
+for i in "animais":
+    print("animais:")
