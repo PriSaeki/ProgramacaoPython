b840a71790282920dd443ecc7c3f886a818b6eab
PriSaeki <priscilasaeki@gmail.com>
2025-06-17 21:57:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import meu_modulo
+# import meu_modulo as mm
+# from meu_modulo import *
+# from meu_modulo import atividade1
+
+
+print(meu_modulo.atividade1())
+meu_modulo.atividade2()
+meu_modulo.atividade3()
+
---
+++ meu_modulo.py
@@ -0,0 +1,29 @@
+# import random
+
+# def atividade1():
+#     al = random.randint(5,10)
+#     return al
+
+# def atividade2():
+#     al1 = random.randint(1,10)
+#     al2 = random.randint(2,10)
+#     al3 = random.randint(3,10)
+#     print(al1, al2, al3)
+
+# def atividade3():
+#     al  = random.randrange(10,30)
+#     print(al)
+
+# def atividade4():
+#     for n in range(10, 0, -1):
+#      print(n)
+#     print("FOGO!")
+# atividade4
+
+def atividade5():
+     n = int(input('Digite um valor inteiro'))
+for n  in range(10):
+    print(n)
+l=[]
+soma=l/2=0
+atividade5
